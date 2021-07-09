aa46a15a46296b71395fc908325e2ed6227c57ca
TemikXx <the.temik@mail.ru>
2021-07-09 20:34:09 +0300

    lalalala

---
+++ cards/app.js
@@ -0,0 +1,18 @@
+
+function slidesPlugin(activeSlider=3){
+    const slides =document.querySelectorAll('.slide')
+    slides[activeSlider].classList.add('active')
+for (const slide of slides) {
+    slide.addEventListener('click',()=>{
+        clearActiveClasses()
+        slide.classList.add('active');
+    })
+}
+function clearActiveClasses(){
+    slides.forEach((slide)=>{
+slide.classList.remove('active')
+    })
+}
+
+}
+slidesPlugin(4)
